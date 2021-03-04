a9c2930026011f41fe4601b0c46ae6c72232910b
glen <glena072@gmail.com>
2021-03-04 16:50:55 +1000

    updated cart

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,6 @@
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v4.1.1">
-    <title>my coffee</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/starter-template/">
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link href="/docs/4.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/1e6705f353.js" crossorigin="anonymous"></script>
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-     
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-
-    </style>
-    <!-- Custom styles for this template -->
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">
-  </head>
-  <body class="mb-4">
-      <!-- Image and text -->
-    <nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.html">
-      <img src="storage/img/nostamp.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-      my coffee
-    </a>
-    <ul class="navbar-nav ml-auto">
-        <li class="nav-item nav-right dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <!-- <a class="dropdown-item" href="#"><i class="far fa-folder"> Admin</i></a> -->
-              <a class="dropdown-item" href="#"><i class="fas fa-cog"> Settings</i></a>
-              <a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt"> logout</i></a>
-            </div>
-          </li>
-    </ul>
-  </nav>
-{{-- bottom Nav --}}
-  <nav class="navbar navbar-expand-md fixed-bottom navbar-dark bg-dark justify-content-between mt-4">
-      <!-- <a class="nav-link" href="index.html"><i id="homeicon" class="fa fa-home"><span class="sr-only">(current)</span></i></a> -->
-      <a class="nav-link" href="orders.html"><i id="ordersicon" class="fas fa-dollar-sign fa-lg"></i></a>
-      <a class="nav-link" href="cards.html"><i class="fas fa-id-card fa-lg"></i></a>
-      <a class="nav-link" href="favourites.html"><i id="favicon" class="fas fa-coffee fa-lg"></i></a>
-</nav>
-
-
-<main role="main" class="container mb-5">
+@extends('layout.app')
+@section('content')
+<main role="main" class="container mb-5 mt-4">
     <div class="register-view d-flex flex-row justify-content-between card-header mb-3 mt-4">
         <div class="">
             <img src="/storage/img/nostamp.png" width="50" height="50" alt="">
@@ -103,7 +38,7 @@
               </div>
               @enderror
           </div>
-          <div class="mb-4">
+          <div class="mb-4 mt-2">
               <div class="flex">
                   <input type="checkbox" name="remember" id="remember" class="mr-2">
                   <label for="remember">Remember Me</label>
@@ -122,11 +57,12 @@
           </div>
         </div> --}}
         <div class="col-12">
-            <button class="btn btn-primary mt-2" type="submit">Login</button>
+            <button class="btn btn-success btn-block mt-2" type="submit">Login</button>
           </div>
       </form>
     </div>
 </main><!-- /.container -->
+@endsection
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script></body>
