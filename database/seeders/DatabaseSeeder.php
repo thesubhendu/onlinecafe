9e87ed85b0bb7694d43476837e7adc85b7def027
subhendu <bhattasubhendu@gmail.com>
2021-10-16 22:29:59 +0545

    seeder fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(5)->create();
+
+        $this->call(RolesTableSeeder::class);
+        $this->call(UserRolesTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(PermissionRoleTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(VendorsTableSeeder::class);
+
         Product::factory(15)->create();
         $this->call(DataTypesTableSeeder::class);
         $this->call(DataRowsTableSeeder::class);
@@ -24,14 +32,7 @@ class DatabaseSeeder extends Seeder
         $this->call(PagesTableSeeder::class);
         $this->call(MenusTableSeeder::class);
         $this->call(MenuItemsTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(UserRolesTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
-        $this->call(PermissionRoleTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
-
-        $this->call(UsersTableSeeder::class);
-        $this->call(VendorsTableSeeder::class);
         $this->call(OrdersTableSeeder::class);
     }
 }
