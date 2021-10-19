0a2434b14810377e6f9233ef41155d9b6f71abe3
glena072 <glena072@gmail.com>
2021-10-19 11:42:04 +1000

    test blade componets

--- app/Http/Controllers/Account/Subscriptions/SubscriptionSwapController.php
+++ app/Http/Controllers/Account/Subscriptions/SubscriptionSwapController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Account\Subscriptions;
 
-use App\Http\Controllers\Controller;
+use App\Models\Plan;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class SubscriptionSwapController extends Controller
 {
