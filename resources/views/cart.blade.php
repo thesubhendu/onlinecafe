ad99ab61c85a20be06a889be149fe2429f4af75a
subhendu <bhattasubhendu@gmail.com>
2021-11-03 08:38:10 +0545

    cart page design

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,138 +1,161 @@
 <x-app-layout>
-    <div class="container py-4 mb-5">
-        <div class="row>">
-            <div class="cart-index">
-                <div class="mt-2">
-                    @if (session()->has('success_message'))
-                <div class="alert alert-success">
-                    {{ session()->get('success_message') }}
-                </div>
-                @endif
-            </div>
-            @if(count($errors) > 0)
-            <div id="cart_error" class="d-none alert alert-danger" role="alert">
-                <ul>
-                    @foreach ($error->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
-            {{-- cart --}}
-            @if (Cart::count() > 0)
-                <div class="cart-section container"> <!--new cart-->
-                    <div>
-                        <h2>{{ Cart::count() }}  {{ Str::plural('item', Cart::count())}} in Shopping Cart</h2>
-                        <div class="mt-4">
+    <!-- ORDERED ITEMS TABLE -->
+    <section class="ordered-food-item ">
+        <div class="container">
+            <!-- TABLE ITEMS  -->
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>{{ Cart::count() }}  {{ Str::plural('item', Cart::count())}} in Shopping Cart</h2>
+
+                    <div class="table-responsive">
+                        <!-- TABLE -->
+                        <table class="table">
+                            <!-- TABLE HEAD -->
+                            <thead>
+                            <tr>
+                                <th class="item-remove"></th>
+                                <th class="item-thumbnail">PRODUCT</th>
+                                <th class="item-name"></th>
+                                <th class="description"></th>
+                                <th class="price-box">PRICE</th>
+                                <th class="item-quantity">QUANTITY</th>
+                                {{--                                <th class="item-subtotal">TOTAL </th>--}}
+
+                            </tr>
+                            </thead>
+
+                            <!-- TABLE BODY -->
+                            <tbody>
                             @foreach(Cart::content() as $item)
-                            <div class="row border-bottom border-top border-secondary">
-                                <div class="col d-flex justify-content-between mr-3">
-                                    <a href="#"><img src="{{asset('storage/img/nostamp.png')}}" alt="item" class="cart-table-img" style="max-width: 50px; max-width: 50px;"></a>
-                                    <div class="order-details ml-3">
-                                        <div class="cart-table-item px-3 btn"><a href="">{{ $item->name }}</a></div>
-                                        <div class="cart-table-description"><small class="text-muted">{{$item->options->milk}}, Sugar - {{$item->options->sugar}}, Syrup - {{$item->options->syrup}}</small></div>
-                                    </div>
-                                    <div class="d-flex align-items-center justifiy-content-between">
-                                        <div class="d-flex flex-column">
-                                            <form action="{{ route('cart.remove', $item->rowId) }}" method="post">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn"><i class="fa fa-trash mb-1 text-danger"></i></button>
-                                            </form>
-                                            {{-- <a class="btn text-danger"><small>Remove</small></a> <!--form in laravel--> --}}
-                                            <a class="btn"><small><i class="fas fa-cart-arrow-down"></i></small></a> <!--form in laravel-->
-                                        </div>
-                                        <div class="form-group mt-3">
+
+                                <!-- ITEM ROW -->
+                                <tr class="item-row">
+                                    <td class="item-remove ">
+                                        <a href="#" class="remove-item">×</a>
+                                    </td>
+                                    <td class="item-thumbnail">
+                                        <img src="{{asset('assets/images/cappuccino.jpg')}}" alt="">
+                                    </td>
+                                    <td class="item-name">{{$item->name}}</td>
+                                    <td class="description">
+                                        {{$item->options->milk}}, Sugar - {{$item->options->sugar}}, Syrup
+                                        - {{$item->options->syrup}}
+                                    </td>
+                                    <td class="price-box"> ${{$item->price}} </td>
+                                    <td class="item-quantity">
+                                        <div class="control-btn ">
                                             <select class="form-control form-select" id="cartQuantity">
-                                            <option selected>{{$item->qty}}</option>
-                                            <option {{ old('quantity') == "1" ? 'selected' : ''}} value="1">1</option>
-                                            <option {{ old('quantity') == "2" ? 'selected' : ''}} value="2">2</option>
-                                            <option {{ old('quantity') == "3" ? 'selected' : ''}} value="3">3</option>
-                                            <option {{ old('quantity') == "4" ? 'selected' : ''}} value="4">4</option>
-                                            <option {{ old('quantity') == "5" ? 'selected' : ''}} value="5">5</option>
+
+                                                @foreach([1,2,3,'4',5] as $qty)
+                                                    <option
+                                                        value="{{$qty}}" {{$qty == $item->qty ? 'selected' : ''}}>{{$qty}}</option>
+                                                @endforeach
+
                                             </select>
                                         </div>
-                                        <div class="col mt-3">$ {{ $item->price }}</div>
-                                    </div>
-                                </div>
-                            </div> <!-- end cart-table-row -->
-                            @endforeach
-                        </div> <!-- end cart-table -->
-                            <a href="#" class="have-code text-secondary px-2"><small>Have a Code?</small></a>
-                            <div class="input-group mb-3" style="max-width: 100%;">
-                                <input type="text" class="form-control" placeholder="" aria-label="Recipient's username" aria-describedby="button-addon2">
-                                <div class="input-group-append">
-                                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Apply</button>
-                                </div>
-                            </div>
-                            </div> <!-- end have-code-container -->
-                        <div class="row cart-totals bg-light">
-                            <div class="col cart-totals-left alert alert-danger">
-                                Please note that payment will be required on collection from "(vendor name goes here)"
-                            </div>
+                                    </td>
+                                    {{--                                    <td class="item-subtotal">$5.0</td>--}}
+                                </tr>
 
-                            <div class="col cart-totals-right align-items-start">
-                                <div>
-                                    <span>Subtotal</span> <span class="float-right">$<?php echo Cart::subtotal(); ?></span><br>
-                                    <span>Tax</span> <span class="float-right">$<?php echo Cart::tax(); ?></span><br>
-                                    <span>Total</span><span class="float-right">$<?php echo Cart::total(); ?></span>
-                                    <br>
+                            @endforeach
+
+                            </tbody>
+
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- CART TOTAL -->
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="col cart-totals-left alert alert-danger">
+                        Please note that payment will be required on collection from "(vendor name goes here)"
+                    </div>
+                </div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <div class="coupen">
+                        <h4>DO YOU HAVE A PROMO CODE ?</h4>
+                        <form action="">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder=" Promo Code">
+                                <a href="#" class="btn btn-primary">
+                                    APPLY PROMO
+                                </a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-4 ">
+                    <div class="payment-final">
+                        <div class="row cart-totals-row">
+                            <div class="col-md-12">
+                                <div class="cart-totals-title">
+                                    <h3>CART TOTALS</h3>
                                 </div>
                             </div>
-                        </div> <!-- end cart-totals -->
-                        <hr>
-                        <div class="cart-buttons d-flex justify-content-between">
-                            @foreach(Cart::content() as $vendor)
-                                <a href="{{route('vendor.products', $vendor->model->vendor_id )}}"
-                                   class="btn btn-outline-success">Continue Shopping</a>
-                            @endforeach
-                            <a href="{{route('checkout.index')}}" class="btn btn-success">Proceed to Checkout</a>
                         </div>
-                </div>
-                @else
-                    <h3> Your Cart is Empty </h3>
-                    <div class="cart-buttons d-flex justify-content-between">
-                        <a href="/" class="btn btn-outline-success">Continue Shopping</a>
-                    </div>
-                @endif
-            </div><!--end of new cart-->
-            {{-- Save for later --}}
-            @if (Cart::instance('saveForLater')->count() > 0)
-                <div class=container>
-                    <div class="row">
-                        <h3 class="mt-4">Saved for later</h3>
-                        @foreach(Cart::instance('saveForLater')->content() as $item)
-                            <div class="d-flex saveForLater mx-auto mb-5">
-                                <div class="card mt-4" style="width: 5rem;">
-                                    <div class="mr-1">
-                                        <img src="{{asset('storage/img/nostamp.png')}}" class="" alt="product"
-                                             width="70" height="70">
-                                    </div>
-                                    {{-- <img src="{{asset('storage/img/nostamp.png')}}" class="card-img-top" alt="product" width="70"> --}}
-                                    <div class="card-body">
-                                    <h5 class="card-title">{{$item->name}}</h5>
-                                    <p class="card-text">${{$item->price}}</p>
+                        <div class="row cart-totals-row">
+                            <div class="col-md-6">
+                                <div class="cart-totals-title">
+                                    <h4>Subtotal</h4>
                                 </div>
-                                <div class="d-flex">
-                                    <form action="{{ route('saveforlaer.addtocart', $item->rowId) }}" method="post">
-                                        @csrf
-                                        <button type="submit" class="btn btn-default"><i class="fas fa-cart-plus"></i></button>
-                                    </form>
-                                    <form action="{{ route('saveforlater.remove', $item->rowId) }}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-default"><i
-                                                class="fa fa-trash mb-1 text-danger"></i></button>
-                                    </form>
-                                </div>
-                                </div>
-                                @endforeach
                             </div>
-                    </div>
-                    @else
+                            <div class="col-md-6">
+                                <div class="cart-totals-result">
+                                    <h4>${{Cart::subtotal()}}</h4>
+                                </div>
+                            </div>
+                        </div>
 
-                    @endif
+
+                        <div class="row cart-totals-row">
+                            <div class="col-md-6">
+                                <div class="cart-totals-title">
+                                    <h4>Tax</h4>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="cart-totals-result">
+                                    <h4 class="final-pay">${{Cart::tax()}}</h4>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row cart-totals-row">
+                            <div class="col-md-6">
+                                <div class="cart-totals-title">
+                                    <h4>Total</h4>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="cart-totals-result">
+                                    <h4 class="final-pay">${{Cart::total()}}</h4>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row ">
+
+                            <div class="col-md-12">
+                                <div class="cart-totals-result">
+
+                                    <a href="{{route('checkout.index')}}" class="btn btn-secondary mt-25">
+                                        CHECKOUT
+                                    </a>
+                                    <a href="{{route('home')}}" class="btn btn-default d-block">
+                                        Back to Shopping
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-        </div> {{--end cart row --}}
-    </div>{{-- end of contatiner--}}
+            </div>
+
+        </div>
+    </section>
 </x-app-layout>
