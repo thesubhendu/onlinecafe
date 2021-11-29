6f1d578d8775d19850fa5d741d8f2a83da0319b1
subhendu <bhattasubhendu@gmail.com>
2021-11-29 08:58:32 +0545

    added owl carosal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,16 @@
     <link rel="stylesheet" href="{{asset('/assets/css/font-awesome.min.css')}}">
 <!-- <link rel="stylesheet" href="{{asset('/assets/css/styles.css')}}">
     <link rel="stylesheet" href="{{asset('/assets/css/responsive.css')}}"> -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     @laravelPWA
 
     @livewireStyles
     <script src="https://js.stripe.com/v3/"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
@@ -36,12 +42,19 @@
 <x-message></x-message>
 
 <!-- Page Content -->
-    {{ $slot }}
+{{ $slot }}
 
-    @stack('modals')
+@stack('modals')
 
-    @livewireScripts
+@livewireScripts
 
-        @stack('scripts')
-    </body>
+@stack('scripts')
+
+
+<script>
+    $(document).ready(function () {
+        $('.owl-carousel').owlCarousel();
+    });
+</script>
+</body>
 </html>
