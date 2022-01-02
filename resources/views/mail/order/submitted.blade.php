ccf63e5ff8f0b8667897db2e0ab885e8828e3e30
subhendu <bhattasubhendu@gmail.com>
2022-01-02 19:43:22 +0545

    order submitted mail changes

--- resources/views/mail/order/submitted.blade.php
+++ resources/views/mail/order/submitted.blade.php
@@ -1,40 +1,41 @@
 @component('mail::message')
-# Your customer
-<img class="img-fluid rounded-start" src="{{asset('storage/img/user/default_user.jpg')}}" alt="" height="50" width="50">
+    # Your customer
+    <img class="img-fluid rounded-start" src="{{asset('storage/img/user/default_user.jpg')}}" alt="" height="50"
+         width="50">
 
-{{auth()->user()->name}}
-# Submitted Order {{$order->id }}
-# please click on the link below to confirm receipt of the order, will then message {{auth()->user()->name}} to come a
-collect the order.
-@component('mail::panel')
-<div class="container">
-@foreach($order->products as $product)
-<div class="card" style="width: 540px;">
-<div class="row g-0">
-<div class="col-md-4">
-<img class="img-fluid rounded-start" src="{{asset('storage/img/nostamp.png')}}" alt="" height="50"
-width="50">
-</div>
+    {{auth()->user()->name}}
+    # Submitted Order {{$order->id }}
+    # Please click on the link below to confirm receipt of the order and view details, will then message {{auth()->user()->name}} to come and collect the order.
+    @component('mail::panel')
+        <div class="container">
+            @foreach($order->products as $product)
+                <div class="card" style="width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img class="img-fluid rounded-start" src="{{asset('storage/img/nostamp.png')}}" alt=""
+                                 height="50"
+                                 width="50">
+                        </div>
 
-<div class="col-md-8">
-<div class="card-body">
-<h5 class="card-title">New Order</h5>
-<p class="card-text">{{$product->pivot->quantity}} x {{$product->name}}</p>
-@foreach (json_decode($product->pivot->options, true) as $key => $value )
-{{$key}} == {{$value}}
-@endforeach
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">New Order</h5>
+                                <p class="card-text">{{$product->pivot->quantity}} x {{$product->name}}</p>
+                                @foreach (json_decode($product->pivot->options, true) as $key => $value )
+                                    {{$value}} <br>
+                                @endforeach
 
-<p class="card-text">Total: ${{$product->pivot->price}}</p>
-</div>
-</div>
-</div>
-</div>
-@endforeach
-</div>
-@endcomponent
+                                ## Total: ${{$product->pivot->price}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endcomponent
 
-@component('mail::button', ['url' => $url])
-Confirm Order
+    @component('mail::button', ['url' => $url])
+        Confirm Order
 @endcomponent
 
 Thanks,<br>
