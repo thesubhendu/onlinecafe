730901f25cf538dd53aabb1fbc6e2a91acef414d
subhendu <bhattasubhendu@gmail.com>
2022-01-05 21:52:44 +0545

    added features title

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -41,50 +41,51 @@
                             </div>
                         </div>
                     </div>
-            <div class="row no-gutters">
-                <div class="col-md-3 col-xs-12">
-                    <div class="icon-box">
+                    <div class="row no-gutters">
+                        <div class="col-md-3 col-xs-12">
+                            <div class="icon-box">
+                                <h4>No queues</h4>
+                                <div class="icon-image">
+                                    <img src="{{asset('assets/images/queue.png')}}" alt="">
+                                </div>
+                                <div class="icon-title">
+                                    <h3>Avoid the queues by ordering ahead</h3>
+                                </div>
 
-                        <div class="icon-image">
-                            <img src="{{asset('assets/images/queue.png')}}" alt="">
+                            </div>
                         </div>
-                        <div class="icon-title">
-                            <h3>No queues- avoid the queues by ordering ahead</h3>
+                        <div class="col-md-3 col-xs-12">
+                            <div class="icon-box">
+                                <h4>Rewards and deals</h4>
+                                <div class="icon-image">
+                                    <img src="{{asset('assets/images/trophy-bw.png')}}" alt="">
                         </div>
+                                <div class="icon-title">
+                                    <h3>All your rewards and deals in one spot. No more carrying multiple coffee loyalty
+                                        cards,
+                                        they are all tracked here in one place
+                                    </h3>
+                                </div>
 
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-12">
-                    <div class="icon-box">
-
-                        <div class="icon-image">
-                            <img src="{{asset('assets/images/trophy-bw.png')}}" alt="">
+                            </div>
                         </div>
-                        <div class="icon-title">
-                            <h3>All your rewards and deals in one spot. No more carrying multiple coffee loyalty cards,
-                                they are all tracked here in one place
-                            </h3>
+                        <div class="col-md-3 col-xs-12">
+                            <div class="icon-box">
+                                <h4>Go cashless</h4>
+                                <div class="icon-image">
+                                    <img src="{{asset('assets/images/online-payment.png')}}" alt="">
+                                </div>
+                                <div class="icon-title">
+                                    <h3>Go cashless with safe and secure online payments</h3>
+                                </div>
+
+                            </div>
                         </div>
-
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-12">
-                    <div class="icon-box">
-
-                        <div class="icon-image">
-                            <img src="{{asset('assets/images/online-payment.png')}}" alt="">
-                        </div>
-                        <div class="icon-title">
-                            <h3>Go cashless with safe and secure online payments</h3>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-12">
-                    <div class="icon-box">
-
-                        <div class="icon-image">
-                            <img src="{{asset('assets/images/donation.png')}}" alt="">
+                        <div class="col-md-3 col-xs-12">
+                            <div class="icon-box">
+                                <h4>Feel Good</h4>
+                                <div class="icon-image">
+                                    <img src="{{asset('assets/images/donation.png')}}" alt="">
                         </div>
                         <div class="icon-title">
                             <h3> Feel good with a pay it forward scheme.Pay your reward coffees forward to someone yu
