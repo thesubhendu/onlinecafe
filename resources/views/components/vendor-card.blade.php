80f6c4a4447eeb9a745a43715401f897ebc481e6
subhendu <bhattasubhendu@gmail.com>
2022-01-30 23:57:52 +0545

    made services dynamic

--- resources/views/components/vendor-card.blade.php
+++ resources/views/components/vendor-card.blade.php
@@ -13,13 +13,11 @@
             <a href="{{route('vendor.show', $vendor)}}">
                 <h3>{{$vendor->shop_name ?? $vendor->vendor_name}}</h3>
             <!-- <p><i class="fa fa-map-marker"></i> {{$vendor->address}}, {{$vendor->state}}</p> -->
-                <p class="service-item"><span>Coffee</span> &nbsp; <span>Tea</span> &nbsp; <span>Bakery</span> &nbsp
-                    <span>Cake</span>&nbsp;<span>Coffee</span> &nbsp; <span>Tea</span> &nbsp; <span>Bakery</span> &nbsp
-                    <span>Cake</span></p>
-                {{--                <span class="service-badge"><i class="ti-location-pin"></i>&nbsp; 12.8km </span>--}}
-                {{--                <span class="service-badge"><i class="ti-timer"></i>&nbsp; 3min </span>--}}
-                {{--                <span class="service-badge"><i class="ti-heart"></i>&nbsp; 240 </span>--}}
-                {{--                <span class="service-badge"><i class="ti-time"></i> &nbsp; {{$vendor->is_open ? "Open Now": "Closed"}} </span>--}}
+                <p class="service-item">
+                    @foreach($vendor->services ?? [] as $service)
+                        <span>{{$service}}</span> &nbsp;
+                    @endforeach
+                     </p>
 
 
             </a>
