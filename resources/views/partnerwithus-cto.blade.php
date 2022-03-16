2fbc862823f0ade63ab2254388b68059bb273bbb
Sudesh Bhandari <sudeshbhandari555@gmail.com>
2022-03-16 09:21:31 +0545

    Changed ladning page features, section bg and footer backgrounds

--- resources/views/partnerwithus-cto.blade.php
+++ resources/views/partnerwithus-cto.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div>
-                <h2 class="text-white">Want to sell online?</h2>
+                <h2>Want to sell online?</h2>
                 <p>Partner with us in 3 easy steps</p>
                 <a href="{{ route('register-business.create') }}">
                     <button class="btn btn-primary btn-block btn-lg btn-get-started">Partner with us</button>
