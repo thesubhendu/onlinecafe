d6d37de3de9e14b0777408c7a0cd3fca865f2ac9
Rupesh87 <rupandroo@gmail.com>
2022-05-30 09:18:25 +0545

    added mail to attribute

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,51 +4,14 @@
             <div class="row">
                 <div class="col-md-4">
                     <div class="footer-content">
-                        <h2>Location</h2>
-                        <ul class="contact-list">
-                            <li>
-                                Envato
-                                Level 13, 2 Elizabeth
-                                Victoria 3000
-                                Australia
-                            </li>
-
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="footer-content">
                         <h2>Contact</h2>
                         <ul class="contact-list">
-
                             <li>
-                                <i class="fa fa-phone"></i> 9851016391
-                            </li>
-                            <li>
-                                <i class="fa fa-envelope"></i> support@mycoffees.com.au
-                            </li>
-                            <li>
-
+                                <i class="fa fa-envelope"></i>  <a href="mailto:support@mycoffees.com.au">someone@example.com</a>
                             </li>
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="footer-content">
-                        {{--                        <h2>Opening Hours</h2>--}}
-                        <ul class="contact-list">
-                            <li>
-                                We work from Monday to Friday.
-                            </li>
-                            {{--                            <li>--}}
-                            {{--                                From 8:00 to 18:00--}}
-                            {{--                            </li>--}}
-
-
-                        </ul>
-                    </div>
-                </div>
-
             </div>
         </div>
 
