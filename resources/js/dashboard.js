04bf82c7ae116b04596abcb07dc07373d425a951
Rupesh87 <rupandroo@gmail.com>
2022-07-27 23:07:50 +0545

    updated routes for broadcast

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -8,7 +8,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    forceTLS: true,
 });
 
 window.realtimeSetup = false;
