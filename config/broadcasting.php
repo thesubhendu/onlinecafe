0dbfa6729d99bec032342b90d49c668b4db1b296
subhendu <bhattasubhendu@gmail.com>
2022-08-04 16:33:44 +0545

    config changes

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,8 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
+                'port'=>'443',
+                'scheme'=>'wss'
             ],
         ],
 
