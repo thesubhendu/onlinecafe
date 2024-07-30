b8bc7ebab1be36c3a816a22a9b658871d86fe4b4
subhendu <bhattasubhendu@gmail.com>
2024-07-30 20:52:32 +0545

    added logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,7 +33,8 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Violet,
             ])
             ->brandName(config('app.name'))
-//            ->brandLogo(asset('assets/images/coffee-cup.png'))
+            ->brandLogo(asset('images/logo.jpeg'))
+            ->brandLogoHeight('50px')
             ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
